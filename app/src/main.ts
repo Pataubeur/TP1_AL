676c28bd6c44e013241a22ec84f853ea913a224d
FenrirWolf4566 <bdezordo@gmail.com>
2023-01-13 10:36:30 +0100

    Home mail

--- app/src/main.ts
+++ app/src/main.ts
@@ -7,7 +7,9 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors(
-         { 'origin':'http://localhost:4200' }
+         { 'origin':'http://localhost:4200'  }
+        // { 'origin':'http://localhost:4200' }
+
        );
 
   app.use(helmet());
@@ -20,6 +22,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
  
-  await app.listen(3000);
+  await app.listen(80);
 }
 bootstrap();
